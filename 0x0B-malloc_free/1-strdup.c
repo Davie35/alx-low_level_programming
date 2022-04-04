fcedc60b9020000577be9f505452b644b9c88ead
Davie35 <dnjoroge35@gmail.com>
2022-04-04 22:25:14 +0300

    function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,30 +1,68 @@
 #include "main.h"
-#include <stdlib.h>
-#include <stdio.h>
+/**
+ *_strlen - count array
+ *@s: array of elements
+ *Return: 1
+ */
+
+	int _strlen(char *s)
+	{
+	unsigned int i;
+
+	i = 0;
+	while (s[i] != '\0') /*count character of string*/
+	{
+	i++;
+	}
+
+	return (i);
+	}
 
 /**
- * _strdup - allocates space in memory
- * @str: input string
- *
- * Return: returns a pointer to a newly
- * allocated space in memory
+ *_strcpy - copy arrays
+ *@src: array of elements
+ *@dest: dest array
+ *Return: dest
+ */
+
+	char *_strcpy(char *dest, char *src)
+	{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+	dest[i] = src[i];
+	i++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+	}
+
+/**
+ *_strdup - array for prints a string
+ *@str: array of elements
+ *Return: pointer
  */
 
 	char *_strdup(char *str)
 	{
-	char *duplicate_num;
-	int i = 0, len = 0;
+	char *dst;
+	unsigned int size;
 
-	duplicate_num = malloc(sizeof(char) * (len + 1));
-	if (str == 0 || duplicate_num == NULL)
+	if (str == 0)
 	{
 	return (NULL);
 	}
-	for (i = 0; str[i]; i++)
+
+	size = _strlen(str) + 1;
+
+	dst = (char *) malloc(size *sizeof(char));
+
+	if (dst == 0)
 	{
-	duplicate_num[i] = str[i];
-	len++;
+	return (NULL);
 	}
-	duplicate_num[len] = '\0';
-	return (duplicate_num);
+	_strcpy(dst, str);
+	return (dst);
 	}
